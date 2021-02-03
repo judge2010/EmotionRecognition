55299243282bb12ef4e0e8ae08963d36e07acdc8
judge2010 <74088083+judge2010@users.noreply.github.com>
2021-02-03 15:51:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+Webcam.set({
+    width:250,
+    heiht:250,
+    img_format:'png',
+    png_quality:90
+});
+camera = document.getElementById("view");
+Webcam.attach(camera);
+
+function Take_photo(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("snapshot").innerHTML = "<img id='Photo' src='"+data_uri+"'>"
+    } )
+};
+console.log("ml5",ml5.version);
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/Hu_QVXSmt/model.json",modelLoaded);
+function modelLoaded(){
+ console.log("model has been loaded");
+};
+function Identify(){
+ var img=document.getElementById("Photo");
+ classifier.classify(img,gotResult);
+};
+function gotResult(error,result){
+    if(error){
+        console.log(error);
+    }
+    else{
+    console.log(result);
+    
+    document.getElementById("result_of_prediction1").innerHTML= result[0].label;
+    document.getElementById("result_of_prediction2").innerHTML= result[1].label;
+    }
+}
